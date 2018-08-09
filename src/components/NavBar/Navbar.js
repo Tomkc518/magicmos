e5921242b02593a673489633b3a95efe768b6133
Tomkc518 <32521131+Tomkc518@users.noreply.github.com>
2018-08-08 20:56:33 -0500

    editing nav bar and added google font

--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -2,14 +2,14 @@ import React from "react";
 import "./NavBar.css";
 
 const NavBar = props => (
-  <nav className="navbar navbar-light bg-light">
+  <nav className="navbar navbar-light bg-light sticky-top">
       <a className="navbar-brand">Magic MOs</a>
-      <span class="navbar-text">
+      <span className="navbar-text ml-auto">
         Where Average People Meet Magic! Also sometimes other Things!
       </span>
-      <form className="form-inline">
-        <input className="form-control mr-sm-2" type="search" placeholder="Search for Cards" aria-label="Search"></input>
-        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      <form className="form-inline ml-sm-auto">
+        <input className="form-control mr-sm-1 mr-0" type="search" placeholder="Search for Cards" aria-label="Search"></input>
+        <button className="btn my-2 my-sm-0" type="submit">Search</button>
       </form>
   </nav>
 );
